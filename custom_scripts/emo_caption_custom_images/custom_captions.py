dc536c41f5f8fcdf04d213d141657de2116f2054
lyrs <fleurdelyrs@gmail.com>
2022-03-29 02:09:12 +0200

    update custom_captions with emotion grounding

--- custom_scripts/emo_caption_custom_images/custom_captions.py
+++ custom_scripts/emo_caption_custom_images/custom_captions.py
@@ -4,6 +4,7 @@
 # In[14]:
 
 
+from importlib import import_module
 import os
 import subprocess
 
@@ -25,10 +26,14 @@ preprocessed_data_dir = os.path.join(artemis_repo_dir, "preprocessed_artemis")
 #where is the pretrained model?
 model_path = os.path.join(artemis_repo_dir, "pretrained_models", "emo_grounded_model", "checkpoints","best_model.pt")
 
+#where is the image to emotion classifier?
+img2emo = os.path.join(artemis_repo_dir, "pretrained_models", "img2emo","img2emo.pt")
 
 #parent folder for this script
 custom_caption_dir = os.path.join(artemis_repo_dir, "custom_scripts","emo_caption_custom_images")
 
+# this file will be input for emotion detector
+custom_emo_csv = os.path.join(custom_caption_dir,"temporary_files","custom_emo_images.csv")
 
 # this file will be input for the captioner
 custom_img_csv = os.path.join(custom_caption_dir,"temporary_files","custom_images.csv")
@@ -64,14 +69,80 @@ filenames
 # In[20]:
 
 
-#write the CSV
-with open(custom_img_csv, 'w') as custom_csv:
-    custom_csv.write('image_file') #header
+############ write the CSV for emotion detection
+with open(custom_emo_csv, 'w') as custom_csv:
+    custom_csv.write(",".join(['image_file'])) #header
     custom_csv.write('\n')
     #file paths:
     for filename in filenames:
-        custom_csv.write(filename)
+        custom_csv.write(",".join([filename]))
         custom_csv.write('\n')
+
+
+############ emotion detection
+
+    #load the model and utilities
+from artemis.in_out.neural_net_oriented import image_transformation
+from artemis.in_out.neural_net_oriented import torch_load_model
+from artemis.emotions import IDX_TO_EMOTION
+
+import pandas as pd
+import numpy as np
+from PIL import Image
+import torch
+
+# device = torch.device("cuda:" + str(0) if torch.cuda.is_available() else "cpu") # it is not working with gpus, only cpu :/
+device =torch.device("cpu")
+model = torch_load_model(img2emo, map_location=device)   
+transformation = image_transformation(255)['train']
+
+emotions_df = { # after emotion detection, to be conferted to a dataframe and saved as csv
+    'image_file':[], #filenames
+    'grounding_emotion':[], #main emotion
+}
+
+for i in range(len(IDX_TO_EMOTION)): # for ranked emotions
+    emotions_df['emo' + str(i) ] = []
+    
+    #load the filenames
+filename_df = pd.read_csv(custom_emo_csv)
+filenames = filename_df['image_file']
+
+
+    # go through each image and classify it
+for filename in filenames:
+    with Image.open(filename).convert('RGB') as img:
+
+        img = transformation(img).unsqueeze(0)# unsqueeze to add artificial first dimension
+        
+        # emotion detection
+        emotion_vector = model(img) # apply the model
+        emotion_vector = np.exp(emotion_vector.detach().numpy()) # calculate probabilities
+        sorted_indices = (-emotion_vector).argsort()[0] # sort from most to least likely
+        emotions = [IDX_TO_EMOTION[ind] for ind in sorted_indices]
+
+        #construct the csv line
+        emotions_df['image_file'].append(filename)
+        emotions_df['grounding_emotion'].append(emotions[0])
+        for i in range(len(emotions)):
+            emotions_df['emo' + str(i)].append(emotions[i])
+
+
+
+############ save detected emotions to a CSV for captioning
+pd.DataFrame(emotions_df).to_csv(custom_img_csv)
+"""
+with open(custom_img_csv, 'w') as custom_csv:
+#    custom_csv.write(",".join(['image_file'])) #header
+    custom_csv.write(",".join(['image_file','grounding_emotion', 'emotion','test_1'])) #header
+    custom_csv.write('\n')
+    #file paths:
+    for filename in filenames:
+#        custom_csv.write(",".join([filename]))
+        custom_csv.write(",".join([filename, "awe", "anger", "there"]))
+        custom_csv.write('\n')
+"""
+
 
 
 # # Edit configuration file
@@ -124,15 +195,10 @@ process = subprocess.run(
 
 # # Process pickled output into CSV
 
-# In[ ]:
+results_df = pd.read_pickle(pickle_out_file)
 
+#add the detected emotions
+for i in range(len(IDX_TO_EMOTION)):
+    results_df['emo'+ str(i)] = emotions_df['emo'+ str(i)]
 
-import pickle
-#from IPython.display import display
-import pandas as pd
-
-#from IPython.display import display
-#from PIL import Image
-
-df = pd.read_pickle(pickle_out_file)
-df.to_csv(csv_out_file)
+results_df.to_csv(csv_out_file)
